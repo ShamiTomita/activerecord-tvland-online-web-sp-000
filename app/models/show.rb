1369851ae4e16252206f929b5e545a8df688b3cf
Shami Tomita <shamitomita@gmail.com>
2021-04-21 20:46:31 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,10 @@
 class Show < ActiveRecord::Base
   has_many :characters 
   has_many :actors, through: :characters
+  
+  def actors_list
+    self.characters.collect do |character|
+      character.actor.full_name
+    end 
+  end 
 end
