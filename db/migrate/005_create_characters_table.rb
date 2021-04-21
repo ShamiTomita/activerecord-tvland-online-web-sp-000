5426aeecf19be8513cae0049f8d0279ae2e5e63f
Shami Tomita <shamitomita@gmail.com>
2021-04-21 20:26:49 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters_table.rb
+++ db/migrate/005_create_characters_table.rb
@@ -2,7 +2,8 @@ class CreateCharacters < ActiveRecord::Migration[5.2]
   def change 
     create_table :characters do |t|
       t.string :name 
-      t.string :las 
+      t.integer :actor_id 
+      t.integer :show_id
     end 
   end 
 end 
